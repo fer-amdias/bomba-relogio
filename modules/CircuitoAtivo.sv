6ec551c0e3cf1bd368a96ec7298f102d905c31d3
Mediador09 <fer.amdias@gmail.com>
2025-11-30 19:28:01 -0300

    feat: update CircuitoAtivo module to have reset button, as well as clearer logic

--- modules/CircuitoAtivo.sv
+++ modules/CircuitoAtivo.sv
@@ -1,11 +1,13 @@
 module CircuitoAtivo (
+	input logic RESET,
 	input logic START,
 	input logic ACERTOU_SENHA_B,
 	output logic ENABLE
 );
 
-always_ff @(posedge START, posedge ACERTOU_SENHA_B) begin
-	ENABLE <= ~ACERTOU_SENHA_B;
+always_ff @(posedge START, posedge ACERTOU_SENHA_B, posedge RESET) begin
+	if (RESET | ACERTOU_SENHA_B) ENABLE <= 1'b0;
+	else if (START) ENABLE <= 1'b1;
 end
 
 endmodule
