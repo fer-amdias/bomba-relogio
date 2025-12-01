feae62ab470b26f7908d6f5d8d36cffbed38f79e
Mediador09 <fer.amdias@gmail.com>
2025-12-01 07:29:20 -0300

    fix: bit extension logic for LEDG

--- modules/ExtensaoDeBitsLEDG.sv
+++ modules/ExtensaoDeBitsLEDG.sv
@@ -3,6 +3,6 @@ module ExtensaoDeBitsLEDG (
 	output logic [0:7] LEDG_OUT
 );
 
-assign LEDG_OUT = LEDG_IN; //faz a extensao de sinal por si so
+assign LEDG_OUT = {8{LEDG_IN}}; //faz a extensao de sinal por si so
 
 endmodule
