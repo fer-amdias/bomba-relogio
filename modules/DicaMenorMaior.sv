88262b73b3f5c3cd5e0c0aa407bb7aa41cb3032d
Mediador09 <fer.amdias@gmail.com>
2025-12-01 07:30:19 -0300

    feat: implement DicaMenorMaior circuit

--- modules/DicaMenorMaior.sv
+++ modules/DicaMenorMaior.sv
@@ -1,10 +1,24 @@
 module DicaMenorMaior (
 	input logic ENABLE,
+	input logic ENTER,
 	input logic [0:3] TENTATIVA,
 	input logic [0:3] A,
 	input logic [0:2] B,
 	input logic ACERTOU_SENHA_A,
 	output logic MENOR_OU_MAIOR
 );
+
+always_ff @(posedge ENTER) begin
+	if (ENABLE) begin
+		if (ACERTOU_SENHA_A) begin
+			if (TENTATIVA > B) MENOR_OU_MAIOR <= 1;
+			else MENOR_OU_MAIOR <= 0;
+		end
+		else begin
+			if (TENTATIVA > A) MENOR_OU_MAIOR <= 1;
+			else MENOR_OU_MAIOR <= 0;
+		end
+	end
+end
 
 endmodule
